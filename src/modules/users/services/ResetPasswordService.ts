33cf985f702d402888b541ba0ecaa74dbae8fd92
Diqxy1 <Diqxy1@gmail.com>
2021-02-05 10:09:01 -0300

    added reset and forgot passowrd service and controllers

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,43 @@
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import { hash } from 'bcryptjs';
+
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists.', 404);
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.', 404);
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.', 408);
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.', 404);
+    }
+
+    const token = await userTokensRepository.generate(user.id);
+
+    console.log(token);
+  }
+}
+
+export default SendForgotPasswordEmailService;
